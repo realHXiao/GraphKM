c60067cd4f6439d29586f21dd62a18533fe26f21
He Xiao <71002556+realHXiao@users.noreply.github.com>
2023-09-16 10:26:21 +0800

    Update sabio_KM_unisubstrate_mutant.py

--- Clean_data_codes/sabio_KM_unisubstrate_mutant.py
+++ Clean_data_codes/sabio_KM_unisubstrate_mutant.py
@@ -1,16 +1,3 @@
-#     else :
-#     # if 'mutant' in desc or 'mutated' in desc:
-#         print(desc)
-#         mutant = re.findall('[A-Z]\d+[A-Z]', desc)  # re is of great use
-#         if len(mutant) >=1 :
-#             enzymeType = '/'.join(mutant)
-
-#     if enzymeType :
-#         enzymeType_entries.append(enzymeType)
-
-# # print(enzymeType_entries)
-# print(len(enzymeType_entries))  
-
 import re
 
 with open("./KM_sabio_clean_unisubstrate.tsv", "r", encoding='utf-8') as file :
@@ -29,7 +16,6 @@ for line in lines :
     UniprotID = data[6]
     Value = data[7]
     Unit = data[8]
-    Reference = data[9]
     if 'wildtype' in EnzymeType :
         enzymeType = 'wildtype'
     else :
@@ -40,15 +26,15 @@ for line in lines :
 
     print(enzymeType)
     if enzymeType :
-        clean_mutant.append([Type, ECNumber, Substrate, enzymeType, PubMedID, Organism, UniprotID, Value, Unit, Reference])
+        clean_mutant.append([Type, ECNumber, Substrate, enzymeType, PubMedID, Organism, UniprotID, Value, Unit])
 
 
 # print(enzymeType_entries)
-print(len(clean_mutant))  # 17384
+print(len(clean_mutant)) 
 
 
 with open("./KM_sabio_clean_unisubstrate_2.tsv", "w") as outfile :
-    records = ['Type', 'ECNumber', 'Substrate', 'EnzymeType', 'PubMedID', 'Organism', 'UniprotID', 'Value', 'Unit', 'Reference']
+    records = ['Type', 'ECNumber', 'Substrate', 'EnzymeType', 'PubMedID', 'Organism', 'UniprotID', 'Value', 'Unit']
     outfile.write('\t'.join(records) + '\n')
     for line in clean_mutant :
         outfile.write('\t'.join(line) + '\n')
